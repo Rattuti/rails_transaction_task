d443ed17fb0bd3bea8af03a0775fda8618e73017
Rattuti <i.ishiharar.96296@gmail.com>
2022-06-03 20:28:20 +0900

    second

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,10 +11,12 @@ class OrdersController < ApplicationController
 
   def create
     @order = current_user.orders.build(order_params)
-    @order.save
+    @order.with_lock do
     @order.update_total_quantity
     # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
     redirect_to orders_path
+    @order.save!
+    end
   end
 
   private
